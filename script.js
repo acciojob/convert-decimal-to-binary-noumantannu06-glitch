8849b63680cd9916a4632976aa06a46518ac0097
Md Nouman <noumantannu06@gmail.com>
2026-06-03 18:52:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function decimalToBinary(num) {
   // your code here
-	 return n === 0 ? '0' : n.toString(2);
+  return num === 0 ? '0' : num.toString(2);
 }
 const num = parseInt(prompt("Enter a number."));
 alert(decimalToBinary(num));
